0b5e2d5fac1ddab629c2af98da6614841b868adf
tomikorzu <tomykorzu@icloud.com>
2024-07-26 17:10:31 -0300

    feat(main.js): add function clickInCard

--- main.js
+++ main.js
@@ -6,3 +6,27 @@ let currentPlayer = 'X';
 
 const xImageSrc = '../img/x.png';
 const oImageSrc = '../img/Blue_circle.png';
+
+function clickInCard(event) {
+    const card = event.target;
+    if (card.innerHTML === '' && !card.classList.contains('disabled')) {
+        const img = document.createElement('img');
+        img.src = currentPlayer === 'X' ? xImageSrc : oImageSrc;
+        img.alt = currentPlayer;
+        img.style.width = '100%';
+        img.style.height = '100%';
+        card.appendChild(img);
+        requestAnimationFrame(() => {
+            img.classList.add('visible'); // Añadir la clase para la transición
+        });
+        card.classList.add('disabled');  // Deshabilitar la tarjeta inmediatamente
+        if (checkWin()) {
+            winnerText.textContent = `El ganador es: ${currentPlayer}`;
+            gameMessage.textContent = 'Presione reiniciar';
+            disableCards();
+        } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
+    }
+}
+
