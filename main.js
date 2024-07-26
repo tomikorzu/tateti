5cb6860e1cdace1b45aa2a98d2551aab6840d37c
tomikorzu <tomykorzu@icloud.com>
2024-07-26 17:11:16 -0300

    feat(main.js): add function checkWin

--- main.js
+++ main.js
@@ -30,3 +30,17 @@ function clickInCard(event) {
     }
 }
 
+function checkWin() {
+    const win = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // filas
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // columnas
+        [0, 4, 8], [2, 4, 6]  // diagonales
+    ];
+    return win.some(step => {
+        return step.every(index => {
+            const card = cards[index];
+            const img = card.querySelector('img');
+            return img && img.alt === currentPlayer;
+        });
+    });
+}
